6e70bc1f7442a3222f3af31eca7c3947a47aaf8d
Aramide Ogundiran <ogundiranaramide0@gmail.com>
2023-12-11 12:25:36 -0500

    This is a commit for the landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { loginWithRedirect } = useAuth0();
+  
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <button onClick={() => loginWithRedirect()}>Log In</button>
       </header>
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-t7qwadx5d8z4v2ib.us.auth0.com"
+    clientId="7rj3EB1K0tr9s4xb6BFsTYzweXDTQGlB"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
