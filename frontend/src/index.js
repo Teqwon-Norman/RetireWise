769947397a67cbdb2b51308845942cc2103b88b5
Teqwon Norman <teqwon.norman@outlook.com>
2023-12-12 22:34:56 -0500

    fixed error with auth0 api

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,28 +2,7 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
-<<<<<<< HEAD
-import reportWebVitals from './reportWebVitals';
-import { Auth0Provider } from '@auth0/auth0-react';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Auth0Provider
-    domain="dev-t7qwadx5d8z4v2ib.us.auth0.com"
-    clientId="7rj3EB1K0tr9s4xb6BFsTYzweXDTQGlB"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Auth0Provider>,
-);
-=======
-
 import './index.css';
->>>>>>> refs/remotes/origin/main
 
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
